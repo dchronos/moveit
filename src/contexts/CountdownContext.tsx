e2c8b2ce329e8c1c1fc7d0ed89d1e1cd4a744dc9
dchronos <projetodc@gmail.com>
2021-02-27 01:28:25 -0300

    ajuste countdown para 25 minutos

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -24,7 +24,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: ChallengesProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
